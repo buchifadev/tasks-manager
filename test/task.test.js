6a980e5362f6ecd074733b0fa95d4c46ada58fef
Favout <favobuchi@outlook.com>
2023-01-14 07:33:41 +0100

    Update tests

--- test/task.test.js
+++ test/task.test.js
@@ -28,6 +28,34 @@ test('should create task for user', async () => {
   expect(task.completed).toEqual(false);
 });
 
+test('Should fetch user tasks', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test('Should not delete other users taks', async () => {
+  const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
+});
+
+test('Should delete user task', async () => {
+  await request(app)
+    .delete(`/tasks/${taskTwo._id}`)
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
 
 test('Should fetch only incomplete tasks ', async () => {
   await request(app)
